06781273b8e16430e3edf436784919254a0c3e74
PBM42 <pbm764927@gmail.com>
2018-03-26 07:28:39 +0200

    chore: update packages

--- webpack.config.js
+++ webpack.config.js
@@ -6,38 +6,37 @@ const isProd = nodeEnv === 'production';
 const libname = 'queries-scheduler';
 
 const config = {
-	entry: './src/index.ts',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'index.bundle.js',
-		library: libname,
-		libraryTarget: 'umd',
-	},
-	resolve: {
-		extensions: ['.js', '.ts', '.json'],
-		alias: rxPaths(),
-	},
-	node: {
-		fs: 'empty',
-	},
-	devtool: isProd ? 'hidden-source-map' : 'cheap-module-eval-source-map',
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: JSON.stringify(nodeEnv),
-			},
-		}),
-		new webpack.optimize.ModuleConcatenationPlugin(),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.ts$/,
-				loader: 'ts-loader',
-				options: { onlyCompileBundledFiles: true, compilerOptions: { outDir: '.' } },
-			},
-		],
-	},
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.bundle.js',
+    library: libname,
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.json'],
+    alias: rxPaths(),
+  },
+  node: {
+    fs: 'empty',
+  },
+  devtool: isProd ? 'hidden-source-map' : 'cheap-module-eval-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(nodeEnv),
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        options: { onlyCompileBundledFiles: true, compilerOptions: { outDir: '.' } },
+      },
+    ],
+  },
 };
 
 module.exports = config;
